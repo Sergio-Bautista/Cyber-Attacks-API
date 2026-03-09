f705c540cd6ff7758a5abf55e2cf4ae0ee8965e6
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2026-03-08 22:42:01 -0500

    future quiz

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+// imports json file
 import attacks  from './attacks.json' with { type:'json'};
 import  express  from 'express';
+
+// lets the server know what external domains are allowed to access it's resources
 import cors from 'cors';
 
+// initiates an express instance and set the port number
 const app = express();
 const PORT = 8000;
 
+// lets the app know tho use json and cors
 app.use(express.json());
 app.use(cors())
 
+// creates the server
 app.get('/', (req, res)=>{
     res.json({
         message : 'Cybersecurity Attacks Educational API',
@@ -15,24 +21,20 @@ app.get('/', (req, res)=>{
     });
 });
 
+// specifies what route the API will use to access the attack info
 app.get("/attacks", (req, res) =>{
     res.json(attacks)
 })
 
+app.get("/quiz", (req, res) =>{
+    res.json({
+        title: "This is a quiz for the website",
+        purpose : " to apply the learning we've done"
+    })
+})
 
 
+// makes the server listen to the specified port 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 });
-
-// if search fynctiuon is implemented 
-/*
-const slugify = (text) => {
-  return text
-    .toLowerCase()      // "SQL Attack" -> "sql attack"
-    .trim()             // Removes accidental spaces at the ends
-    .split(' ')         // Splits into an array: ["sql", "attack"]
-    .filter(word => word !== "") // Removes extra spaces in between words
-    .join('-');         // Joins them back: "sql-attack"
-};
-*/
