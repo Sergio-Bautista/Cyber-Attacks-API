cb6631c99169635a7d2e93857d1a6f9308635597
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2026-03-16 10:32:49 -0500

    get a random question from the given options

--- js/quiz.js
+++ js/quiz.js
@@ -1,37 +1,65 @@
 
 
+
+// Function to generate and return a random attack name
 function getRandomQuestion(questionArray){
+    // get the attacks name from the API
     const attacks = questionArray[0].attacks_quiz
 
+    // get only the name of the attack from the json file
     const attackKeys = Object.keys(attacks)
 
+    // generate a random number to return a ransom name from the json file
     const randomAttackIndex = Math.floor(Math.random() * attackKeys.length)
 
+    // get a random name from the attack list
     const randomAttack = attackKeys[randomAttackIndex]
 
-    const attack = attacks[randomAttack]
+    // const attack = attacks[randomAttack]
 
-    const questions = attack.questions
-    const randomQuestionIndex = Math.floor(Math.random() * questions.length)
+    // const questions = attack.questions
+    // const randomQuestionIndex = Math.floor(Math.random() * questions.length)
 
-    const randomQuestion = questions[randomQuestionIndex];
+    // const randomQuestion = questions[randomQuestionIndex];
 
-    return randomQuestion
+    // return the attack name
+    return randomAttack
 }
 
 
 async function loadQuiz(){
     try{
+        // Makes the request to the API server 
         const response = await fetch("http://localhost:8000/quiz");
+
+        // Save the json response and converts it into an array
         const quizData = await response.json();
         const quizArray = Object.values(quizData)
-        // console.log(quizArray[0].attacks_quiz)
+
+        // get a random attack name
+        const randomAttack = getRandomQuestion(quizArray)
+
+        // get the questions array from the given random attack 
+        const questions = quizArray[0].attacks_quiz[randomAttack].questions
+
+        // Chose a random question from the array of questions
+        const randomQuestionIndex = Math.floor(Math.random() * questions.length)
+
+        // select a question from the array
+        const randomQuestion = questions[randomQuestionIndex]
+
+        // get the correct anser for the given question
+        const correctAnswer = quizArray[0].attacks_quiz[randomAttack].questions[randomQuestionIndex].answer
+
+        // console.log(questions)
+        // console.log(randomQuestionIndex)
+        // console.log(randomQuestion)
+        // console.log(correctAnswer)
 
 
-        const randomQuestion = getRandomQuestion(quizArray)
+    //    console.log(quizArray[0].attacks_quiz[randomAttack].questions[0].answer)
 
-       console.log(randomQuestion)
-
+    //   console.log(quizArray[0].attacks_quiz[randomAttack].questions)
 
     }
     catch(error){
