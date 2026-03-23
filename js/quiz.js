6ffe01efe460bd85de6d4478928702040822b12b
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2026-03-22 19:48:28 -0500

    added overlay to show the questions/answers

--- js/quiz.js
+++ js/quiz.js
@@ -5,25 +5,57 @@ let reviewQuestions = [];
 
 // access the containers for the questions/options
 const questionContainer = document.getElementById("question-container");
+let showScore = document.getElementById("score");
 const optionsContainer = document.getElementById("options");
 const buttonNumber = document.querySelector("button");
 const reviewContainerQuestion = document.getElementById("review-container-question")
-const reviewContainerOptions = document.getElementById("review-container-options")
+const overlay = document.getElementById("overlayInfo");
+// const retryWrongQuestions = document.createElement('button');
+const closeBtn = document.createElement("button");
 
+
+closeBtn.addEventListener('click', ()=>{
+    clearEverything()
+})
 // loads the main function
 buttonNumber.addEventListener('click', loadQuiz);
+// retryWrongQuestions.addEventListener('click', retryWrongQuestionsOnly)
 
-// checks if the questions answered is equal to the questions the user wanted
-function checkMaxQuestions(questionsNumber){
-    // TODO
-    // implement an overlay screen with score and questions wrong
-    
-    // checks if the the questions answered is equal to the user's chosen number
+function clearEverything(){
+    // console.log("everything cleared")
+    reviewQuestions.length = 0;
+    score = 0;
+    numberOfQuestions = 0;
+    buttonNumber.style.display = ""
+    document.querySelector("input").style.display = ''
+    questionContainer.textContent = "";
+    optionsContainer.textContent = "";
+    reviewContainerQuestion.textContent = "";
+    overlay.style.display = 'none'
+}
+
+function displayQuestionsAndAnswers(questionsNumber){
     if(numberOfQuestions === Number(questionsNumber)){
+        // const overlay = document.getElementById("overlayInfo");
+        // const closeBtn = document.createElement("button");
+        closeBtn.className = 'close-button';
+        closeBtn.textContent = "close";
+
+        // const retryWrongQuestions = document.createElement('button');
+        retryWrongQuestions.textContent = 'Retry Wrong Questions';
+        
+        // closeBtn.addEventListener('click', ()=>{
+        //     // overlay.style.display = 'none';
+        //     clearEverything()
+        // })
+        // retryWrongQuestions.addEventListener('click', retryWrongQuestionsOnly)
+
+        reviewContainerQuestion.textContent = "";
+        
         // iterates through the questions the user has answered
         reviewQuestions.forEach(question =>{
             // gets the correct answer for the question
-            const correctAnswer = question.answer            
+            // const correctAnswer = question.answer            
 
             // creates an h2 element to display the question answered for review and adds it to the container
             const showReviewQuestion = document.createElement('h2');
@@ -38,7 +70,7 @@ function checkMaxQuestions(questionsNumber){
                 reviewOption.textContent = option
                 
                 // adds a green border to the correct option 
-                if (option == correctAnswer){
+                if (option === question.answer ){
                     reviewOption.style.border = '2px solid green'
                 }
                 
@@ -48,14 +80,102 @@ function checkMaxQuestions(questionsNumber){
                 }else{
                     showReviewQuestion.classList.add("wrongQuestion");
                     // adds a blue border to the option chosen if the answer is wrong
-                    if(option == question.userAnswer){
-                        reviewOption.style.border = "2px solid blue"
+                    const optionIndex = question.options.indexOf(option);
+
+                    if (!question.answerCorrectly && optionIndex === question.userAnswerIndex) {
+                        reviewOption.style.border = "2px solid blue";
                     }
+                    // if(option == question.userAnswer){
+                    //     reviewOption.style.border = "2px solid blue"
+                    // }
                 };
                 // adds the options to the container
                 reviewContainerQuestion.append(reviewOption)
             })
         });
+        overlay.append(reviewContainerQuestion)
+        overlay.append(showScore)
+        overlay.append(closeBtn)
+        overlay.append(retryWrongQuestions)
+        overlay.style.display = "flex"
+    };
+}
+function retryWrongQuestionsOnly(){
+    console.log("These are the questions you got wrong")
+    const wrongReviewQuestions = reviewQuestions.filter(question =>{
+       return question.answerCorrectly !== true;    
+    })
+    console.log(wrongReviewQuestions)
+    clearEverything()
+
+}
+
+// checks if the questions answered is equal to the questions the user wanted
+function checkMaxQuestions(questionsNumber){
+    // checks if the the questions answered is equal to the user's chosen number
+    if(numberOfQuestions === Number(questionsNumber)){
+        // const overlay = document.getElementById("overlayInfo");
+        // const closeBtn = document.createElement("button");
+        closeBtn.className = 'close-button';
+        closeBtn.textContent = "close";
+
+        // const retryWrongQuestions = document.createElement('button');
+        // retryWrongQuestions.textContent = 'Retry Wrong Questions';
+        
+        // closeBtn.addEventListener('click', ()=>{
+        //     // overlay.style.display = 'none';
+        //     clearEverything()
+        // })
+        // retryWrongQuestions.addEventListener('click', retryWrongQuestionsOnly)
+
+        reviewContainerQuestion.textContent = "";
+        
+        // iterates through the questions the user has answered
+        reviewQuestions.forEach(question =>{
+            // gets the correct answer for the question
+            // const correctAnswer = question.answer            
+
+            // creates an h2 element to display the question answered for review and adds it to the container
+            const showReviewQuestion = document.createElement('h2');
+            showReviewQuestion.textContent = question.question;
+            reviewContainerQuestion.append(showReviewQuestion)
+            
+            // iterates through each question's options 
+            question.options.forEach(option =>{
+
+                // creates a div element to display the options for each question
+                const reviewOption = document.createElement('div')
+                reviewOption.textContent = option
+                
+                // adds a green border to the correct option 
+                if (option === question.answer ){
+                    reviewOption.style.border = '2px solid green'
+                }
+                
+                // checks for the flag and add the class to the element
+                if(question.answerCorrectly){
+                    showReviewQuestion.classList.add("rightQuestion");
+                }else{
+                    showReviewQuestion.classList.add("wrongQuestion");
+                    // adds a blue border to the option chosen if the answer is wrong
+                    const optionIndex = question.options.indexOf(option);
+
+                    if (!question.answerCorrectly && optionIndex === question.userAnswerIndex) {
+                        reviewOption.style.border = "2px solid blue";
+                    }
+                    // if(option == question.userAnswer){
+                    //     reviewOption.style.border = "2px solid blue"
+                    // }
+                };
+                // adds the options to the container
+                reviewContainerQuestion.append(reviewOption)
+            })
+        });
+        overlay.append(reviewContainerQuestion)
+        overlay.append(showScore)
+        overlay.append(closeBtn)
+        // overlay.append(retryWrongQuestions)
+        overlay.style.display = "flex"
     };
 };
 
@@ -63,7 +183,8 @@ function checkMaxQuestions(questionsNumber){
 function clearScreen(){
     questionContainer.textContent = "";
     optionsContainer.textContent = "";
-    // reviewQuestions = [];
+    buttonNumber.style.display = "none"
+    document.querySelector("input").style.display = 'none'
 };
 // Function to generate and return a random attack name
 function getRandomQuestion(questionArray){
@@ -83,19 +204,20 @@ function getRandomQuestion(questionArray){
 };
 async function loadQuiz(){
     // get the number of questions the user wants 
+    clearScreen()
     const questionsNumber = document.querySelector("input").value;
+    // console.log(questionsNumber)
 
     // hides the input field and button
-    buttonNumber.style.display = "none"
-    document.querySelector("input").style.display = 'none'
+    // buttonNumber.style.display = "none"
+    // document.querySelector("input").style.display = 'none'
 
     // calls the clear function every time to place the new question/options
-    clearScreen()
     try{
         // Makes the request to the API server locally 
-        // const response = await fetch("http://localhost:8000/quiz");
+        const response = await fetch("http://localhost:8000/quiz");
         // Makes the request to the API when is hosted in railways
-        const response = await fetch("https://cyber-attacks-api-production.up.railway.app/quiz")
+        // const response = await fetch("https://cyber-attacks-api-production.up.railway.app/quiz")
 
         // Save the json response and converts it into an array
         const quizData = await response.json();
@@ -114,29 +236,33 @@ async function loadQuiz(){
         const randomQuestion = questions[randomQuestionIndex];
 
         // get the correct anser for the given question (just in case)
-        const showCorrectAnswer = randomQuestion.answer;
+        // const showCorrectAnswer = randomQuestion.answer;
 
         // creates an h2 element to place the question
         const showQuestion = document.createElement("h2");
         showQuestion.textContent = randomQuestion.question || "No Data";
 
         // get the correct answer for comparison
-        const correctAnswer = randomQuestion.answer;
+        // const correctAnswer = randomQuestion.answer;
 
         // loops thorough the options of a given question 
-        randomQuestion.options.forEach(option => {
+        randomQuestion.options.forEach((option, index) => {
             // and creates a button with each option in it
             const button = document.createElement("button");
             button.textContent = option;
+            // console.log("option:", option)
+            // console.log("option:", index)
 
             // adds an event listener to each button, and assigns the content of the button to the user's options
             button.addEventListener('click', function(){
                 // adds content of the button to the user option and adds it to the question object 
-                const userOption = this.textContent || "no option";
-                randomQuestion.userAnswer = userOption
+
+                randomQuestion.userAnswerIndex = index;
+                // const userOption = this.textContent || "no option";
+                // randomQuestion.userAnswer = userOption
 
                 // calls the function to check if the user has reach the number of questions wanted
-                checkAnswer(userOption, correctAnswer, questionsNumber, randomQuestion);
+                checkAnswer(index, questionsNumber, randomQuestion);
             });
             // appends the buttons to the option container for display 
             optionsContainer.append(button);
@@ -150,27 +276,33 @@ async function loadQuiz(){
     };
 };
 // keeps track of user score, checks if the questions limits has been reached, and loads the next question
-function checkAnswer(userOption, correctAnswer, questionsNumber, randomQuestion){
-    // access the score of the user for display
-    let showScore = document.getElementById("score");
+function checkAnswer(userIndex, questionsNumber, randomQuestion){
+    
     //  TODO: Implement a better score system
     // adds a boolean property value to the question object 
-    randomQuestion.answerCorrectly = userOption === correctAnswer
+    const correctIndex = randomQuestion.options.indexOf(randomQuestion.answer)
+    randomQuestion.answerCorrectly = userIndex === correctIndex;
+
+    // randomQuestion.answerCorrectly = 
+    // userOption.trim().toLowerCase() === 
+    // randomQuestion.answer.trim().toLowerCase();
     
     // checks if the answer is correct or not to add points
-    if(userOption == correctAnswer){
+    if(randomQuestion.answerCorrectly){
         score+=10;
     }else{
         score-=1;
     };
+    // console.log("Before push:", reviewQuestions);
     reviewQuestions.push(randomQuestion)
+    // console.log("After push:", reviewQuestions);
     // adds one to the number of questions
-    numberOfQuestions += 1;
+    numberOfQuestions ++;
     // shows the score in the scree
     showScore.textContent = score;
 
     // check if the number of questions has been reached
-    checkMaxQuestions(questionsNumber, userOption);
+    checkMaxQuestions(questionsNumber);
     // calls the main function to load a new question
     loadQuiz();
 };
